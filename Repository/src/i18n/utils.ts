9a6bf6c374effa249a45e77b2e22449c7d7f3f11
Balita <carlbalita@mode83.onmicrosoft.com>
2025-11-06 12:15:20 +0100

    utils update

--- Repository/src/i18n/utils.ts
+++ Repository/src/i18n/utils.ts
@@ -3,51 +3,48 @@ import FRLang from './fr.json';
 import ESLang from './es.json';
 
 export const languages = {
-	en: 'English',
-	fr: 'Français',
-	es: 'Español',
+  en: 'English',
+  fr: 'Français',
+  es: 'Español',
 };
 
 export const defaultLang = 'fr';
 
-export const ui = {
-	en: flattenObject(ENLang),
-	fr: flattenObject(FRLang),
-	es: flattenObject(ESLang),
-} as const;
-
 function flattenObject(ob: any) {
-	const toReturn: any = {};
-
-	for (const i in ob) {
-		if (!ob.hasOwnProperty(i)) continue;
-
-		if (typeof ob[i] === 'object' && ob[i] !== null) {
-			const flatObject = flattenObject(ob[i]);
-			for (const x in flatObject) {
-				if (!flatObject.hasOwnProperty(x)) continue;
-
-				toReturn[i + '.' + x] = flatObject[x];
-			}
-		} else {
-			toReturn[i] = ob[i];
-		}
-	}
-	return toReturn;
+  const toReturn: any = {};
+  for (const i in ob) {
+    if (!Object.prototype.hasOwnProperty.call(ob, i)) continue;
+    if (typeof ob[i] === 'object' && ob[i] !== null) {
+      const flatObject = flattenObject(ob[i]);
+      for (const x in flatObject) {
+        if (!Object.prototype.hasOwnProperty.call(flatObject, x)) continue;
+        toReturn[i + '.' + x] = flatObject[x];
+      }
+    } else {
+      toReturn[i] = ob[i];
+    }
+  }
+  return toReturn;
 }
 
+export const ui = {
+  en: flattenObject(ENLang),
+  fr: flattenObject(FRLang),
+  es: flattenObject(ESLang),
+} as const;
+
 export function getLangFromUrl(url: URL) {
-	const [, lang] = url.pathname.split('/');
-	if (lang in ui) return lang as keyof typeof ui;
-	return lang as keyof typeof ui;
+  const [, maybeLang] = url.pathname.split('/');
+  return (maybeLang in ui ? maybeLang : defaultLang) as keyof typeof ui;
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-	return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-		return ui[lang][key] || ui[defaultLang][key];
-	};
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+    const langSet = ui[lang] || ui[defaultLang];
+    return langSet[key] || ui[defaultLang][key] || `[missing: ${String(key)}]`;
+  };
 }
 
 export async function getStaticPaths() {
-	return Object.keys(languages).map((lang) => ({ params: { lang } }));
+  return Object.keys(languages).map((lang) => ({ params: { lang } }));
 }
