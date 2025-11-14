e37140391c16463b31046fd6e53e58e2f3967cd4
Balita <carlbalita@mode83.onmicrosoft.com>
2025-11-14 16:45:57 +0100

    debugged productcard

--- Repository/src/env.d.ts
+++ Repository/src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+declare global {
+  interface Window {
+    dispatchAddToCartModal?: (product: any) => void;
+    addToCart?: (product: any) => void;
+    updateCartBadge?: () => void;
+    clearCart?: () => void;
+  }
+}
+
+export {};
+
